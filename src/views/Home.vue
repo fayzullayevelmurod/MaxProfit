7f4cc9448d2584f958b451ceb4fd77b31f8e2d1e
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-01-27 04:46:44 +0500

    footer the end

---
+++ src/views/Home.vue
@@ -0,0 +1,335 @@
+<template>
+  <div>
+
+    <!-- message -->
+    <section class="message">
+      <div class="main_container">
+        <h4>
+          MaxProfit Message Live
+        </h4>
+        <div class="message_in">
+          <div class="message_left">
+            <p>
+              Этот инструмент — идеальное решение для отслеживания ключевых запросов в реальном времени. 
+            </p>
+            <p>
+              Представьте, что вы являетесь держателем монеты, о которой скоро появятся новости. Решение о покупке или продаже ваших активов может зависеть от того, будут ли новости положительными или отрицательными. Наш Live Scanner позволяет вам легко настраивать отслеживание ключевых слов и чатов, предоставляя вам информацию ещё до официальных публикаций в СМИ. 
+            </p>
+            <p>
+              Получайте доступ к новостям  и принимайте решения оперативно, чтобы максимизировать свою прибыль, основываясь на инсайдах рынка.
+            </p>
+          </div>
+          <div class="message_center">
+            <img src="@/assets/images/messenger_img.png" alt="">
+          </div>
+          <ul class="message_right">
+            <li>
+              <img src="@/assets/images/icons/check_messange.svg" alt="">
+              Настройка бота занимает меньше минуты
+            </li>
+            <li>
+              <img src="@/assets/images/icons/check_messange.svg" alt="">
+              Бот работает в реальном времени
+            </li>
+            <li>
+              <img src="@/assets/images/icons/check_messange.svg" alt="">
+              Настройка по ключевым словам
+            </li>
+            <li>
+              <img src="@/assets/images/icons/check_messange.svg" alt="">
+              Использование бота не только для криптовалютной сферы 
+            </li>
+            <li>
+              <img src="@/assets/images/icons/check_messange.svg" alt="">
+              Предоставление клиентам списка проверенных крипто-чатов
+            </li>
+            <li>
+              <img src="@/assets/images/icons/check_messange.svg" alt="">
+              Получение инсайдерской информации
+            </li>
+            <li>
+              <img src="@/assets/images/icons/check_messange.svg" alt="">
+              Персональные уведомления которые присылаются клиентам в отдельный чат
+            </li>
+          </ul>
+        </div>
+        <div class="message_btns">
+          <button class="main_btn">
+            Начать пользоваться
+          </button>
+        </div>
+      </div>
+    </section>
+    <!-- message -->
+
+    <!-- products -->
+    <section class="products">
+      <div class="products_bg"></div>
+      <div class="main_container">
+          <h3>
+            Как использовать продукты MaxProfit 
+          </h3>
+          <p>
+            Это всесторонняя экосистема для арбитража криптовалют вы знаете, что в условиях волатильности рынка и при быстро меняющихся курсах токенов на различных биржах, разница может достигать 50% в круг. Отслеживать это вручную – задача трудная и трудоемкая.
+          </p>
+          <p>
+            MaxProfit был разработан для экономии вашего времени. Бот автоматически мониторит курсы токенов на разных биржах. С его помощью вы можете быстро определить разницу в ценах между биржами  и совершить прибыльные арбитражные сделки. 
+          </p>
+          <p>
+            MaxProfit Message поможет вам в связке с арбитражным ботом находить самые прибыльные арбитражные возможности на данный момент. 
+          </p>
+          <h6>
+            А Анализ кошельков от MaxProfit - это лучшая аналитика on-chain данных на рынке с помощью искусственного интеллекта. А также специальные инструменты, для поиска наиболее профитных кошельков и их сделок на рынке.
+          </h6>
+          <div class="products_imgs">
+            <img src="@/assets/images/products_img.png" alt="">
+          </div>
+          <div class="products_bottom_text">
+            <img src="@/assets/images/products_mini_img.png" alt="">
+            <div class="products_bottom_right">
+              <p>
+                MaxProfit - это абсолютно безопасный инструмент для вашего капитала. Мы стремимся обеспечить максимальную защиту ваших средств, поэтому наш бот полностью ручной и не требует подключения к балансам ваших бирж по API. Никаких дополнительных рисков – вы абсолютно уверены в сохранности своих денег.
+              </p>
+              <p>
+                Бот работает в облаке, обеспечивая вам доступ к актуальной информации в режиме реального времени. Без необходимости предоставлять доступ к вашим кошелькам, вы с уверенностью можете использовать MaxProfit для выявления и осуществления прибыльных арбитражных возможностей на рынке. Надежность и безопасность вашего капитала – наш приоритет.
+              </p>
+            </div>
+          </div>
+      </div>
+    </section>
+    <!-- products -->
+
+    <!-- works -->
+    <!-- <section class="works">
+
+    </section> -->
+    <!-- works -->
+
+    <!-- tarif -->
+    <section class="tarif">
+      <div class="tarif_bg"></div>
+      <div class="main_container">
+        <div class="tarif_title">
+          <h3>
+            Тарифы
+          </h3>
+        </div>
+        <div class="tarif_cards">
+          <div class="tarif_card">
+            <div class="tarif_card_head">
+              <h5>
+                MaxProfit<br>
+                Silver
+              </h5>
+              <del>
+                100 $
+              </del>
+              <h4>
+                0$
+              </h4>
+            </div>
+            <ul>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Access to the exclusive event
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Welcome cocktails
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                DJ<br> zone
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Opportunity to join the advertiser's table, where food, drinks, and alcohol will be complimentary for you
+              </li>
+            </ul>
+            <button class="main_btn">
+              Купить тариф
+            </button>
+          </div>
+          <div class="tarif_card gold_card">
+            <div class="tarif_card_head">
+              <h5>
+                MaxProfit<br>
+                Gold
+              </h5>
+              <del>
+                100 $
+              </del>
+              <h4>
+                100$
+              </h4>
+            </div>
+            <ul>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Access to the exclusive event
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Welcome cocktails
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                DJ<br> zone
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Opportunity to join the advertiser's table, where food, drinks, and alcohol will be complimentary for you
+              </li>
+            </ul>
+            <button class="main_btn">
+              Купить тариф
+            </button>
+          </div>
+          <div class="tarif_card">
+            <div class="tarif_card_head">
+              <h5>
+                MaxProfit<br>
+                Platinum
+              </h5>
+              <del>
+                100 $
+              </del>
+              <h4>
+                100$
+              </h4>
+            </div>
+            <ul>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Access to the exclusive event
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Welcome cocktails
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                DJ<br> zone
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_blue.svg" alt="">
+                Opportunity to join the advertiser's table, where food, drinks, and alcohol will be complimentary for you
+              </li>
+            </ul>
+            <button class="main_btn">
+              Купить тариф
+            </button>
+          </div>
+          <div class="tarif_card blue_card">
+            <div class="tarif_card_head">
+              <h5>
+                MaxProfit<br>
+                VIP
+              </h5>
+              <del>
+                100 $
+              </del>
+              <h4>
+                100$
+              </h4>
+            </div>
+            <ul>
+              <li>
+                <img src="@/assets/images/icons/check_green.svg" alt="">
+                Access to the exclusive event
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_green.svg" alt="">
+                Welcome cocktails
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_green.svg" alt="">
+                DJ<br> zone
+              </li>
+              <li>
+                <img src="@/assets/images/icons/check_green.svg" alt="">
+                Opportunity to join the advertiser's table, where food, drinks, and alcohol will be complimentary for you
+              </li>
+            </ul>
+            <button class="main_btn">
+              Купить тариф
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- tarif -->
+
+    <!-- program -->
+    <section class="program">
+      <div class="main_container">
+        <div class="program_left">
+          <h4 class="program_title">
+            Партнерская программа
+          </h4>
+          <h6>
+            Мы готовы платить до 50% с каждой продажи. Самая щедрая партнерская программа.
+          </h6>
+          <p>
+            Если вы имеете свой Ютуб, Телеграм канал, сайт, блог и тд, то мы предлагаем Вам партнерскую систему работы. Свяжитесь с нами в Телеграм <a href="#">@ник</a> или по почте <a href="#">почта@почта.com</a>
+          </p>
+        </div>
+        <div class="program_right">
+          <div class="program_img_bg">
+            <img src="@/assets/images/program_img.png" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- program -->
+  </div>
+</template>
+
+<script>
+// import { refs } from 'vue'
+// import Swiper, { Navigation, Pagination, Autoplay } from 'swiper'
+// import 'swiper/css';
+
+// // import required modules
+// export default {
+//   name: "Home",
+//   data() {
+//     return {
+//       activeIndex: 0,
+//     }
+//   },
+//   mounted() {
+//     const SECOND = 1000 // milliseconds
+
+//     new Swiper(this.$refs.swiper, {
+
+//       modules: [Navigation, Pagination, Autoplay],
+
+//       loop: true,
+//       autoplay: {
+//         delay: 3 * SECOND,
+//         disableOnInteraction: false,
+//       },
+//       speed: 2 * SECOND,
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//       },
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+
+//       on: {
+//         slideChange: (swiper) => {
+//           this.activeIndex = swiper.realIndex
+//         },
+//       },
+//     })
+//   },
+// }
+</script>
+
+<style>
+
+</style>
